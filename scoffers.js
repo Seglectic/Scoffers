83e5cc0f9f1427e3447f0c58cf64d2c6e622c6ec
Segger <saintdane@gmail.com>
2015-10-09 22:00:35 -0400

    Scoffers 1.2
    Made more object oriented, everything is contained within Scoffers
    object and stream is started once instantiated.

--- scoffers.js
+++ scoffers.js
@@ -1,62 +1,81 @@
+/*
+
+			Norman Scoffers, Bot Butler
+
+*/
+
 var twitter = require("twitter");
 var keys = require("./config.json");
 
-
-var scoffers = new twitter(keys);
-
-console.log("Norman Scoffers v1.0")
-
-
-
-
-
 /*
-	Gratifies a joke with a "Good one, sir." to whomever
-	the author of tweet is.
+			General purpose methods
+		Section for general methods not
+		tied to any specific object
 */
 
-var goodOne = function(tweet){
-	//Actual message to send:
-	var message = "@"+tweet.user.screen_name+"\nGood one, sir.";
-	//JSON to send to twitter
-	var post = {
-		status:message,
-		in_reply_to_status_id:tweet.id_str,
-	};
-	//Call the post method, feed in the above post object and run callback when complete.
-	scoffers.post('statuses/update',post,function(error,tweet,response){
-		if(error){
-			console.log(tweet);
-			console.log(response);
-		}else{
-			console.log("Hilarity appealed.");
-		}
-	});
+//Clear terminal window
+var clear = function(center){
+    process.stdout.write('\033c\n\n\n');
+    //Center cursor
+    if(center){
+		for(var x=0;x<Math.floor(process.stdout.rows/2);x++){process.stdout.write("\n");}
+    }
 }
 
 
-/*
-	Stream through all #joke tagged tweets and respond wittily when
-	Khal posts a joke.
-*/
 
-scoffers.stream("statuses/filter",{track:'#joke',include_retweets:false},function(stream){
-	//Handle #joke datastream
-	stream.on('data',function(tweet){
+Scoffers = function(){
+	clear();
+	
+	version = "1.2";
+	console.log("Norman Scoffers",version)
+	var self = this;
+	this.client = new twitter(keys);
 
-		if(tweet.user.id_str==='40078553'){
-			console.log(tweet.user.name+" made a #joke: \n"+tweet.text);
-			goodOne(tweet);
-		}else{
-			console.log(tweet.user.name,": ",tweet.text);
-		}
-	});
+	//Gratify a #joke with a "Good one, sir."
+	this.goodOne = function(tweet){
+		var message = "@"+tweet.user.screen_name+"\nGood one, sir.";
+		var post = {
+			status:message,
+			in_reply_to_status_id:tweet.id_str,
+		};
+		//Post to twitter
+		self.client.post('statuses/update',post,function(error,tweet,response){
+			if(error){
+				console.log(tweet);
+				console.log(response);
+			}else{
+				console.log("Hilarity gratified.");
+			}
+		});
+	};
+
+	//Datastream callback
+	this.dataStream = function(stream){
+		stream.on('data',function(tweet){
+
+			//Check if has Khal ID
+			if(tweet.user.id_str==='40078553'){
+				console.log(tweet.user.name+" made a #joke: \n"+tweet.text);
+				self.goodOne(tweet);
+			}else{
+				console.log(tweet.user.name,": ",tweet.text);
+			}
+
+		});
+
+		stream.on('error',function(error){
+			clear();
+			console.log(error);
+		});
+	}
+	post = {
+		track:"#joke",
+		include_retweets:false
+	}
+	this.client.stream("statuses/filter",post,this.dataStream)
 
 
-	//Handle errors
-	stream.on('error',function(error){
-		console.log(error);
-	});
+}
 
-});
-
+Scoffers();
